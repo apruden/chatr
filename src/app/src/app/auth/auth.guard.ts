3784a580966105f7a7bebe92f7667e712a8f1860
Alex Prudencio <alex.prudencio@gmail.com>
2020-04-11 10:59:54 -0400

    ionic app

---
+++ src/app/src/app/auth/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '@/app/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
+    let loginState = null;
+
+    try {
+      loginState = await this.auth.getCurrentState();
+      console.log(['state', loginState]);
+    } catch {
+      console.log('>>>>');
+    }
+
+    if (loginState) {
+      return true;
+    } else {
+      this.router.navigate(['auth']);
+    }
+  }
+}
---
+++ src/app/src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'search',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../search/search.module').then(m => m.SearchPageModule)
+          }
+        ]
+      },
+      {
+        path: 'feed',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../feed/feed.module').then(m => m.FeedPageModule)
+          }
+        ]
+      },
+      {
+        path: 'profile',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../profile/profile.module').then(m => m.ProfilePageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/search',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/search',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
